0cba1ba51d3552e811081182c8d76149f432fa78
javad bakhshi <javad bakhshi>
2022-09-21 15:02:30 +0430

    first commit

--- webscraping/views.py
+++ webscraping/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from django.http import HttpResponse
-from .models import Scrape
-from .serializers import ScrapeSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import generics
+from .serializers import ScrapeSerializer
+from .models import Scrape
 
 
-html = requests.get('https://www.digikala.com/search/category-mobile-phone/')
-soup = BeautifulSoup(html.text, 'html.parser')
-name = soup.select('#\32 5 > div > div:nth-child(1) > a > article > div.d-flex.grow-1.pos-relative.flex-row > div.grow-1.d-flex.flex-column.ai-stretch.jc-start')
-
-
+url = "https://www.digikala.com/search/category-mobile-phone/product-list/?price%5Bmax%5D=727221278&price%5Bmin%5D=0&sort=21"
 titles = []
-
-
-for header in name:
-    titles.append(header.text)
-    # titles = titles.strip()
-    
-
+stars = []
+prices = []
+for page in range(1, 3):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    results = soup.find_all('div', attrs={'class':'px-5'})
+    titles = ['']
+    for result in results:
+        title = result.find('div', attrs={'class': 'nth-child'}).find('div', attrs={'class': 'd-flex.grow-1.pos-relative.flex-row'}) # result not results
+        price = result.find('div', attrs={'class': 'nth-child'}).text[13:-11]
+        titles.append((title, price,))
 
 new_rss = Scrape(
-    title = titles, 
+    title = titles,
     url = 'https://www.digikala.com/search/category-mobile-phone/product-list/?price%5Bmax%5D=727221278&price%5Bmin%5D=0&sort=21'
 )
 
